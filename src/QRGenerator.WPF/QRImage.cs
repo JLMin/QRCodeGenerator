3fc6f9d9aaa3be30b7b39edab3bfea5a1a5902df
JLMin <2479297@qq.com>
2020-04-09 19:47:28 +0800

    randomize test image

--- src/QRGenerator.WPF/QRImage.cs
+++ src/QRGenerator.WPF/QRImage.cs
@@ -1,4 +1,5 @@
 ﻿using QRGenerator.Library;
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
@@ -11,26 +12,30 @@ namespace QRGenerator.WPF
     {
         public static Bitmap CreateBitmap(QRSettings settings)
         {
-            Bitmap bitmap = new Bitmap(400, 400);
+            // TODO: from settings
+            int qr_size = 400;
+            int block_size = 20;
+            Color bgcolor = Color.Gray;
+            Brush black = Brushes.Black;
+            Brush white = Brushes.White;
+
+            Bitmap bitmap = new Bitmap(qr_size, qr_size);
             Point p = new Point(0, 0);
             using Graphics graphic = Graphics.FromImage(bitmap);
-            graphic.Clear(Color.Gray);
+            graphic.Clear(bgcolor);
 
-            int[] data = { 1, 0, 1, 0, 1, 0, 1, 0 };
+            int[] data = RandomData((int)Math.Pow(qr_size / block_size, 2));
 
             for (int i = 0; i < data.Length; i++)
             {
-                Brush brush = (data[i] == 1) ? Brushes.Black : Brushes.White;
-                Rectangle rect = new Rectangle(p.X, p.Y, 20, 20);
+                Brush brush = (data[i] == 1) ? black : white;
+                Rectangle rect = new Rectangle(p.X, p.Y, block_size, block_size);
                 graphic.FillRectangle(brush, rect);
-                if (p.X > 400)
+                p.X += block_size;
+                if (p.X >= qr_size)
                 {
                     p.X = 0;
-                    p.Y += 20;
-                }
-                else
-                {
-                    p.X += 20;
+                    p.Y += block_size;
                 }
             }
             return bitmap;
@@ -47,6 +52,17 @@ namespace QRGenerator.WPF
             bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
             bitmapimage.EndInit();
             return bitmapimage;
+        }
+
+        private static int[] RandomData(int size)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(0, 2);
+            }
+            return array;
         }
     }
 }
