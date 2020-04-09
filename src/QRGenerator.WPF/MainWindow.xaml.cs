df9dc03b22f68be7b7b683b07017327188209dce
JLMin <2479297@qq.com>
2020-04-09 19:31:00 +0800

    add a simple gui

---
+++ src/QRGenerator.WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+using QRGenerator.Library;
+
+namespace QRGenerator.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Generate_Click(object sender, RoutedEventArgs e)
+        {
+            //first, create a dummy bitmap just to get a graphics object
+            //using Image bitmap = new Bitmap(1, 1);
+            //using Graphics drawing = Graphics.FromImage(bitmap);
+            //drawing.Clear(Color.Red);
+            //drawing.Save();
+
+            //using MemoryStream memory = new MemoryStream();
+            //bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            //memory.Position = 0;
+            //BitmapImage bitmapimage = new BitmapImage();
+            //bitmapimage.BeginInit();
+            //bitmapimage.StreamSource = memory;
+            //bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            //bitmapimage.EndInit();
+            QRSettings settings = new QRSettings();
+            Bitmap bitmap = QRImage.CreateBitmap(settings);
+            Img_QR.Source = QRImage.BitmapToImageSource(bitmap);
+        }
+    }
+}
---
+++ src/QRGenerator.WPF/QRImage.cs
@@ -0,0 +1,52 @@
+﻿using QRGenerator.Library;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace QRGenerator.WPF
+{
+    public class QRImage
+    {
+        public static Bitmap CreateBitmap(QRSettings settings)
+        {
+            Bitmap bitmap = new Bitmap(400, 400);
+            Point p = new Point(0, 0);
+            using Graphics graphic = Graphics.FromImage(bitmap);
+            graphic.Clear(Color.Gray);
+
+            int[] data = { 1, 0, 1, 0, 1, 0, 1, 0 };
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Brush brush = (data[i] == 1) ? Brushes.Black : Brushes.White;
+                Rectangle rect = new Rectangle(p.X, p.Y, 20, 20);
+                graphic.FillRectangle(brush, rect);
+                if (p.X > 400)
+                {
+                    p.X = 0;
+                    p.Y += 20;
+                }
+                else
+                {
+                    p.X += 20;
+                }
+            }
+            return bitmap;
+        }
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using MemoryStream memory = new MemoryStream();
+            bitmap.Save(memory, ImageFormat.Png);
+            memory.Position = 0;
+            BitmapImage bitmapimage = new BitmapImage();
+            bitmapimage.BeginInit();
+            bitmapimage.StreamSource = memory;
+            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapimage.EndInit();
+            return bitmapimage;
+        }
+    }
+}
